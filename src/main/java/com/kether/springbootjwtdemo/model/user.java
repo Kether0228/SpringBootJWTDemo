dc7fa33f0b77d2251aae08f1572a0157d8b0b8cb
Kether <Kether0228@gmail.com>
2025-04-23 00:57:24 +0800

    feat:user model

--- src/main/java/com/kether/springbootjwtdemo/model/user.java
+++ src/main/java/com/kether/springbootjwtdemo/model/user.java
@@ -1,4 +1,13 @@
 package com.kether.springbootjwtdemo.model;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class user {
+    private Integer id;
+    private String username;
+    private String password;
+    private List<role> roles;
 }
