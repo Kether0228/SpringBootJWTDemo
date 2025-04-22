62bc1a29a09b29d6736f020e0cae1a9b95994b21
Kether <Kether0228@gmail.com>
2025-04-23 01:02:37 +0800

    feat:user service interface

--- src/main/java/com/kether/springbootjwtdemo/service/userService.java
+++ src/main/java/com/kether/springbootjwtdemo/service/userService.java
@@ -1,4 +1,20 @@
 package com.kether.springbootjwtdemo.service;
 
+import com.kether.springbootjwtdemo.dto.userRequest;
+import com.kether.springbootjwtdemo.model.user;
+
+import java.util.List;
+
 public interface userService {
+    Integer adduser(userRequest userRequest);
+    user getUserById(Integer id);
+    List<user> getAllUser();
+    void deleteUserById(Integer userId);
+    user login(String username, String password);
+
+//    void addRole(Integer userId, Integer roleId);
+//    void deleteUser(Integer id);
+//    void updateUser(Integer id, String username, String password, String role);
+//    void updateRole(Integer id, Integer roleId);
+//    void deleteRole(Integer id, Integer roleId);
 }
