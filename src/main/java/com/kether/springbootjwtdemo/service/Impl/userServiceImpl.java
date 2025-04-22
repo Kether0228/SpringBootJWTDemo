9bd18df3302abdf68a9df6e726e4621bb29bbb7c
Kether <Kether0228@gmail.com>
2025-04-23 01:03:02 +0800

    feat:user service interface 實作

--- src/main/java/com/kether/springbootjwtdemo/service/Impl/userServiceImpl.java
+++ src/main/java/com/kether/springbootjwtdemo/service/Impl/userServiceImpl.java
@@ -1,4 +1,65 @@
 package com.kether.springbootjwtdemo.service.Impl;
 
-public class userServiceImpl {
+import com.kether.springbootjwtdemo.dao.userDao;
+import com.kether.springbootjwtdemo.dto.userRequest;
+import com.kether.springbootjwtdemo.model.user;
+import com.kether.springbootjwtdemo.service.userService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Service
+public class userServiceImpl implements userService {
+
+    @Autowired
+    private userDao userDao;
+
+    @Autowired
+    private PasswordEncoder PasswordEncoder;
+
+    @Override
+    public Integer adduser(userRequest userRequest) {
+        user checkExistingUser = userDao.getUserByUsername(userRequest.getUser());
+        if (checkExistingUser != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already exists");
+        }
+        String hashPassword = PasswordEncoder.encode(userRequest.getPassword());
+        userRequest.setPassword(hashPassword);
+
+        return userDao.adduser(userRequest);
+    }
+
+    @Override
+    public user getUserById(Integer userId) {
+        user user = userDao.getUserById(userId);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        return user;
+    }
+
+
+    @Override
+    public List<user> getAllUser() {
+        List<user> userList = userDao.getAllUser();
+        return userList;
+    }
+
+    @Override
+    public void deleteUserById(Integer userId) {
+        userDao.deleteUserById(userId);
+    }
+
+    @Override
+    public user login(String username, String password) {
+        user user = userDao.getUserByUsername(username);
+        if (user != null && PasswordEncoder.matches(password, user.getPassword())) {
+            return user;
+        }
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid username or password");
+    }
 }
