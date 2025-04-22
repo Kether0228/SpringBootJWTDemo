5ed54b2b73d80d8d04b4d695125d58b52cf590d8
Kether <Kether0228@gmail.com>
2025-04-23 00:53:50 +0800

    feat:spring security認證

--- src/main/java/com/kether/springbootjwtdemo/config/SecurityConfig.java
+++ src/main/java/com/kether/springbootjwtdemo/config/SecurityConfig.java
@@ -1,4 +1,26 @@
 package com.kether.springbootjwtdemo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //測試用，關閉所有認證機制
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf().disable(); // optional
+        return http.build();
+    }
+
 }
