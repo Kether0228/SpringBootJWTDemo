57546e1ae628cdcc3bc0cc5ea112cd4a1f416cdf
Kether <Kether0228@gmail.com>
2025-04-23 01:01:53 +0800

    feat:user請求model

--- src/main/java/com/kether/springbootjwtdemo/dto/userRequest.java
+++ src/main/java/com/kether/springbootjwtdemo/dto/userRequest.java
@@ -1,4 +1,12 @@
 package com.kether.springbootjwtdemo.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class userRequest {
+    @NotBlank
+    private String user;
+    @NotBlank
+    private String password;
 }
