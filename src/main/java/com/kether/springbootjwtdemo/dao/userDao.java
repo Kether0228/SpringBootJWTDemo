f8af823b93e71e9117eee1a9f4972135ad797ab4
Kether <Kether0228@gmail.com>
2025-04-23 00:59:24 +0800

    feat:userDao interface 相關功能

--- src/main/java/com/kether/springbootjwtdemo/dao/userDao.java
+++ src/main/java/com/kether/springbootjwtdemo/dao/userDao.java
@@ -1,4 +1,19 @@
 package com.kether.springbootjwtdemo.dao;
 
+
+import com.kether.springbootjwtdemo.dto.userRequest;
+import com.kether.springbootjwtdemo.model.user;
+
+import java.util.List;
+
+
 public interface userDao {
+    Integer adduser(userRequest userRequest);
+    user getUserById(Integer id);
+    List<user> getAllUser();
+    void deleteUserById(Integer userId);
+    void deleteUserRolesByUserId(Integer userId);
+    user getUserByUsername(String username);
+
+//    void setRole(Integer userId, Integer roleId);
 }
