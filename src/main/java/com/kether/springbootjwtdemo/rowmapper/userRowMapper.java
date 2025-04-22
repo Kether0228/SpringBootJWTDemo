aff5570ddecd6956b280d4c4e283ed74878a1769
Kether <Kether0228@gmail.com>
2025-04-23 01:02:15 +0800

    feat:user row mapper

--- src/main/java/com/kether/springbootjwtdemo/rowmapper/userRowMapper.java
+++ src/main/java/com/kether/springbootjwtdemo/rowmapper/userRowMapper.java
@@ -1,4 +1,19 @@
 package com.kether.springbootjwtdemo.rowmapper;
 
-public class userRowMapper {
+import com.kether.springbootjwtdemo.model.user;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class userRowMapper implements RowMapper<user> {
+    @Override
+    public user mapRow(ResultSet rs, int rowNum) throws SQLException {
+        user user = new user();
+        user.setId(rs.getInt("user_id"));
+        user.setUsername(rs.getString("user"));
+        user.setPassword(rs.getString("password"));
+
+        return user;
+    }
 }
