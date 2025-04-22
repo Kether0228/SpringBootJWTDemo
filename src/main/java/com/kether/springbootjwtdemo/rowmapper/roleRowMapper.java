5eb4e3e51058a781986fc636fcf20cb07b3d63d0
Kether <Kether0228@gmail.com>
2025-04-23 00:53:03 +0800

    feat:role rowMapper

--- src/main/java/com/kether/springbootjwtdemo/rowmapper/roleRowMapper.java
+++ src/main/java/com/kether/springbootjwtdemo/rowmapper/roleRowMapper.java
@@ -1,4 +1,17 @@
 package com.kether.springbootjwtdemo.rowmapper;
 
-public class roleRowMapper {
+import com.kether.springbootjwtdemo.model.role;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class roleRowMapper implements RowMapper<role> {
+    @Override
+    public role mapRow(ResultSet rs, int rowNum) throws SQLException {
+        role role = new role();
+        role.setRole_id(rs.getInt("role_id"));
+        role.setRole(rs.getString("role"));
+        return role;
+    }
 }
